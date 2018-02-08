cc5c033dfdbf21eeccbca56d9d02badb3aa31d4e
cmpoon <cmpoon@users.noreply.github.com>
2018-02-08 13:04:18 +0000

    polished git setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install zsh curl powerline
+#for debian ubuntu
+
+#sudo apt-get update
+sudo apt-get install -y git zsh curl powerline
 #zsh-antigen installed below
+
+git config --global user.email "cmpoon@users.noreply.github.com"
+git config --global user.name "cmpoon"
 
 curl -L git.io/antigen > ~/antigen.zsh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -16,5 +21,5 @@ vim +PluginInstall +qall
 #command -v zsh | sudo tee -a /etc/shells
 chsh -s "$(command -v zsh)" "${USER}"
 #chsh -s zsh
-
+echo "Done!"
 zsh
